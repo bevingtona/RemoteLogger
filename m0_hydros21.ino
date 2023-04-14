088e8ecabe158ac16c218053124e417a98d071ff
Alexandre Bevington <alexandre.bevington@gov.bc.ca>
2023-04-14 14:37:38 -0700

    fix irid msg and unit test

--- m0_hydros21.ino
+++ m0_hydros21.ino
@@ -1,76 +1,89 @@
+// LOG FILE
+// MESSAGE AS BYTES
+// READ METRICS CODE FROM SD
+/* A - TEMP, B - LEVEL, C - EC, D - NTU, E - SNOW DEPTH, F - AIR TEMP, G - RH */  
+
+// #define ISBD_SUCCESS             0
+// #define ISBD_ALREADY_AWAKE       1
+// #define ISBD_SERIAL_FAILURE      2
+// #define ISBD_PROTOCOL_ERROR      3
+// #define ISBD_CANCELLED           4
+// #define ISBD_NO_MODEM_DETECTED   5
+// #define ISBD_SBDIX_FATAL_ERROR   6
+// #define ISBD_SENDRECEIVE_TIMEOUT 7
+// #define ISBD_RX_OVERFLOW         8
+// #define ISBD_REENTRANT           9
+// #define ISBD_IS_ASLEEP           10
+// #define ISBD_NO_SLEEP_PIN        11
+
 /*Include the libraries we need*/
 #include "RTClib.h" //Needed for communication with Real Time Clock
-#include <SPI.h> //Needed for working with SD card
-#include <SD.h> //Needed for working with SD card
-#include <ArduinoLowPower.h> //Needed for putting Feather M0 to sleep between samples
-#include <IridiumSBD.h> //Needed for communication with IRIDIUM modem 
-#include <CSV_Parser.h> //Needed for parsing CSV data
-#include <SDI12.h> //Needed for SDI-12 communication
-#include <QuickStats.h>//Needed for commuting median
+#include <SPI.h>//Needed for working with SD card
+#include <SD.h>//Needed for working with SD card
+#include <ArduinoLowPower.h>//Needed for putting Feather M0 to sleep between samples
+#include <IridiumSBD.h>//Needed for communication with IRIDIUM modem 
+#include <CSV_Parser.h>//Needed for parsing CSV data
+#include <SDI12.h>//Needed for SDI-12 communication
+#include <QuickStats.h>//Needed for computing medians
 
 /*Define global constants*/
-const byte led = 13; // Built in led pin
-const byte chipSelect = 4; // Chip select pin for SD card
-const byte irid_pwr_pin = 6; // Power base PN2222 transistor pin to Iridium modem
-const byte PeriSetPin = 5; //Power relay set pin to HYDROS21
-const byte PeriUnsetPin = 10; //Power relay unset pin to HYDROS21
+const byte led = 8; // Built in GREEN LED pin (13 conflicts with irid)
+const byte chipSelect = 4; // For SD card
+const byte IridPwrPin = 11; // Pwr pin to Iridium modem // THIS IS ALSO THE PIN FOR THE RED LED
+const byte SensorSetPin = 5; //Pwr set pin to HYDROS21
+const byte SensorUnsetPin = 6; //Pwr unset pin to HYDROS21
 const byte dataPin = 12; // The pin of the SDI-12 data bus
+// const byte WiperSetPin = 10; // Pin to set pwr relay to Analite 195
+// const byte WiperUnsetPin = 11; // Pin to unset pwr relay to Analite 195
+// const byte TurbAlog = A1; // Pin for reading analog outout from voltage divder (R1=1000 Ohm, R2=5000 Ohm) conncted to Analite 195
 const byte vbatPin = 9;
 
-
 /*Define global vars */
-String header = "datetime, water_level_mm, water_temp_c, water_ec_dcms, batt_v";
-int16_t *sample_intvl;// Sample interval in minutes (Read from PARAM.txt)
-int16_t *irid_freq;// Iridium transmit freqency in hours (Read from PARAM.txt)
-uint32_t irid_freq_hrs;// Iridium transmit freqency in hours
-uint32_t sleep_time;// Logger sleep time in milliseconds
-DateTime present_time;// Var for keeping the current time
-int err;//IRIDIUM status var
-String myCommand   = "";// SDI-12 command var
-String sdiResponse = "";// SDI-12 responce var
+String my_header = "datetime,water_level_mm,water_temp_c,water_ec_dcm,batt_v";
 
-/*Define Iridium seriel communication as Serial1 */
+int16_t *sample_freq_m; //
+uint32_t sample_freq_m_32;// 
+int16_t *irid_freq_h; //
+uint32_t irid_freq_h_32;//
+char **onstart_irid;//
+DateTime present_time;//Var for keeping the current time
+int err; //IRIDIUM status var
+String myCommand   = ""; //SDI-12 command var
+String sdiResponse = ""; //SDI-12 responce var
+
+/*Define Iridium seriel communication COM1*/
 #define IridiumSerial Serial1
 
 /*SDI-12 sensor address, assumed to be 0*/
 #define SENSOR_ADDRESS 0
 
 /*Create library instances*/
-RTC_PCF8523 rtc; // Setup a PCF8523 Real Time Clock instance (may have to change this to more precise DS3231)
+RTC_PCF8523 rtc; // Setup a PCF8523 Real Time Clock instance
 File dataFile; // Setup a log file instance
 IridiumSBD modem(IridiumSerial); // Declare the IridiumSBD object
 SDI12 mySDI12(dataPin);// Define the SDI-12 bus
+QuickStats stats;//Instance of QuickStats
 
 float read_params(){
     //Set paramters for parsing the parameter file PARAM.txt
-  CSV_Parser cp("dd", true, ',');
+  CSV_Parser cp("sdds", true, ',');
 
   /*Read the parameter file 'PARAM.txt', blink (1-sec) if fail to read*/
-  while (!cp.readSDfile("/PARAM.txt"))
-  {
-    digitalWrite(led, HIGH);
-    delay(1000);
-    digitalWrite(led, LOW);
-    delay(1000);
-  }
-
-  sample_intvl = (int16_t*)cp["sample_intvl"];
-  irid_freq = (int16_t*)cp["irid_freq"];
-
-  sleep_time = sample_intvl[0] * 1000;//Sleep time between samples in miliseconds from PARAM.txt
-  irid_freq_hrs = irid_freq[0];  //Iridium transmission frequency in hours form PARAM.txt
+  while (!cp.readSDfile("/PARAM.txt"))  {blinky(1,1000,1000,1000);}
+  
+  sample_freq_m = (int16_t*)cp["sample_freq_m"];
+  sample_freq_m_32 = sample_freq_m[0];
+  Serial.println(sample_freq_m_32);
+  irid_freq_h = (int16_t*)cp["irid_freq_h"];
+  irid_freq_h_32 = irid_freq_h[0];
+  Serial.println(irid_freq_h_32);
+  onstart_irid = (char**)cp["onstart_irid"];
+  Serial.println(onstart_irid[0]);
+  
   return 1;
 }
-float read_vbat() { 
-  pinMode(vbatPin, INPUT); //Set VBAT pin as INPUT
-  return (analogRead(vbatPin)*2*3.3)/1024;
-}
 
-String sample_hydros21(){
-  
-  set_relay();
-
-  delay(1000); //Give HYDROS21 sensor time to settle 
+String sample_hydros_M(){
 
   myCommand = String(SENSOR_ADDRESS) + "M!";// first command to take a measurement
 
@@ -89,7 +102,7 @@ String sample_hydros21(){
   if (sdiResponse.length() > 1)
     mySDI12.clearBuffer();
 
-  delay(1000);       // delay between taking reading and requesting data
+  delay(2000);       // delay between taking reading and requesting data
   sdiResponse = "";  // clear the response string
 
 
@@ -126,48 +139,33 @@ String sample_hydros21(){
   if (sdiResponse.length() > 1)
     mySDI12.clearBuffer();
 
-  //Assemble datastring
-  ;
-
-  /*Switch power off to HYDROS21 via latching relay*/
-  unset_relay();
-
   return sdiResponse;
 }
 
-float set_relay() {
-  pinMode(PeriSetPin, OUTPUT);       //Set peripheral set pin as OUTPUT
-  pinMode(PeriUnsetPin, OUTPUT);     //Set peripheral unset pin as OUTPUT
-  digitalWrite(PeriUnsetPin, HIGH); delay(20); //Drive peripheral unset pin HIGH to assure unset relay
-  digitalWrite(PeriUnsetPin, LOW); delay(20);  //Drive peripheral unset pin LOW as it is a latching relay
-  digitalWrite(PeriSetPin, HIGH); delay(20); //Drive peripheral unset pin HIGH to assure unset relay
-  digitalWrite(PeriSetPin, LOW); delay(20);  //Drive peripheral unset pin LOW as it is a latching relay
+float blinky(int16_t n, int16_t high_ms, int16_t low_ms, int16_t btw_ms){
+  for(int i = 1; i <= n; i++) {  
+    digitalWrite(led, HIGH);
+    delay(high_ms);
+    digitalWrite(led, LOW);
+    delay(low_ms);}
+  delay(btw_ms);  
   return 1;
 }
 
-float unset_relay() {
-  pinMode(PeriSetPin, OUTPUT);       //Set peripheral set pin as OUTPUT
-  pinMode(PeriUnsetPin, OUTPUT);     //Set peripheral unset pin as OUTPUT
-  digitalWrite(PeriUnsetPin, HIGH); delay(20);  //Drive peripheral unset pin HIGH to assure unset relay
-  digitalWrite(PeriUnsetPin, LOW); delay(20);  //Drive peripheral unset pin LOW as it is a latching relay
-  return 1;
+String sample_batt_v(){
+    pinMode(vbatPin, INPUT);
+    String batt_v = String((analogRead(vbatPin)*2*3.3)/1024);
+    return batt_v;
 }
 
-String take_measurement(String present) {
-  float vbat = read_vbat();
-  String hydros = sample_hydros21();
-  String datastring = present + "," + hydros + "," + vbat;
-  return datastring;
-}
-
-float write_to_csv(String datastring_for_csv, String outname) {
+float write_to_csv(String header, String datastring_for_csv, String outname) {
 
   // IF FILE DOES NOT EXIST, WRITE HEADER AND DATA, ELSE, WITE DATA
   if (!SD.exists(outname))  //Write header if first time writing to the logfile
   {
     dataFile = SD.open(outname, FILE_WRITE);  //Open file under filestr name from parameter file
     if (dataFile) {
-      dataFile.println(header+",comment");
+      dataFile.println(header);
       dataFile.println(datastring_for_csv);
     }
     dataFile.close();  //Close the dataFile
@@ -181,152 +179,177 @@ float write_to_csv(String datastring_for_csv, String outname) {
   return 1;
 }
 
-String irid_temp_to_irid(){
-
-  SD.begin(chipSelect);
-  CSV_Parser cp("sffffs", true, ',');  // Set paramters for parsing the log file
-  cp.readSDfile("/TEMP.csv");     // Read IRID.csv file from SD
+String prep_msg(){
   
+  SD.begin(chipSelect);
+  CSV_Parser cp("sffff", true, ',');  // Set paramters for parsing the log file
+  cp.readSDfile("/HOURLY.csv");
   int num_rows = cp.getRowsCount();  //Get # of rows
     
-  char **out_datetimes = (char **)cp["datetime"];        // populate datetimes
-  float *out_water_level_mm = (float *)cp["water_level_mm"];     // populate snow depths
-  float *out_water_temp_c = (float *)cp["water_temp_c"];  // populate air temps
-  float *out_water_ec_dcms = (float *)cp["water_ec_dcms"];               // populate RHs
-  float *out_bat_v = (float *)cp["batt_v"];                // populate V
+  char **out_datetimes = (char **)cp["datetime"];
+  float *out_water_level_mm = (float *)cp["water_level_mm"];
+  float *out_water_temp_c = (float *)cp["water_temp_c"];
+  float *out_water_ec_dcm = (float *)cp["water_ec_dcm"];
+  float *out_batt_v = (float *)cp["batt_v"];
   
-  String datastring_msg = "ABC:" + String(out_datetimes[0]).substring(2, 4) + String(out_datetimes[0]).substring(5, 7) + String(out_datetimes[0]).substring(8, 10) + String(out_datetimes[0]).substring(11, 13) + ":" + String(round(out_bat_v[num_rows-1] * 10)) + ":";
+  String datastring_msg = "ABD:" + String(out_datetimes[0]).substring(2, 4) + String(out_datetimes[0]).substring(5, 7) + String(out_datetimes[0]).substring(8, 10) + String(out_datetimes[0]).substring(11, 13) + ":" + String(round(out_batt_v[num_rows-1] * 100)) + ":";
   
   for (int i = 0; i < num_rows; i++) {  //For each observation in the IRID.csv
-    datastring_msg = datastring_msg + String(round(out_water_level_mm[i])) + ',' + String(round(out_water_temp_c[i]*10)) + ',' + String(round(out_water_ec_dcms[i])) + ':';              
+    datastring_msg = datastring_msg + String(round(out_water_level_mm[i])) + ',' + String(round(out_water_temp_c[i]*10)) + ',' + String(round(out_water_ec_dcm[i])) + ':';              
     }
-  
   return datastring_msg;
-  }
-  
+}
 
-float send_msg(String msg, float timeout_s, float retry_n, String quality){
+float send_msg(String msg, float timeout_s, float retry_n){
 
-  pinMode(irid_pwr_pin, OUTPUT);     //Set iridium power pin as OUTPUT
-  digitalWrite(irid_pwr_pin, HIGH);   //Drive iridium power pin LOW
+  pinMode(IridPwrPin, OUTPUT);     //Set iridium power pin as OUTPUT
+  digitalWrite(IridPwrPin, HIGH);   //Drive iridium power pin LOW
   delay(2000);
   int err;
   
   IridiumSerial.begin(19200);  // Start the serial port connected to the satellite modem
   modem.setPowerProfile(IridiumSBD::USB_POWER_PROFILE); // This is a low power application
   err = modem.begin();
-  
-  if(quality == "T"){
-    int signalQuality = -1;
-    for (int16_t i = 0; i < 5; i++) {
-      err = modem.getSignalQuality(signalQuality);
-      }
-  }
 
   modem.adjustSendReceiveTimeout(timeout_s);
   err = modem.sendSBDText(msg.c_str());  
+
+  int signalQuality = -1;
+  err = modem.getSignalQuality(signalQuality);
+
   if(retry_n > 0){
   for (int16_t i = 0; i < retry_n; i++) {
     if(err != ISBD_SUCCESS)//err != 0 && err != 13)// If first attemped failed try once more with extended timeout
       {
+        delay(1000);        
         modem.setPowerProfile(IridiumSBD::USB_POWER_PROFILE); // This is a low power application
         err = modem.begin(); 
-        modem.adjustSendReceiveTimeout(timeout_s);
-          err = modem.sendSBDText(msg.c_str());
+        err = modem.sendSBDText(msg.c_str());
       }
     }
   }  
-  digitalWrite(irid_pwr_pin, LOW);   //Drive iridium power pin LOW
+  digitalWrite(IridPwrPin, LOW);   //Drive iridium power pin LOW
   return 1;
-  }
-
-
-
-/*
-   The setup function. We only initialize the sensors, RTC, SD and varible states here
-*/
-void setup(void)
-{
-  pinMode(led, OUTPUT);
-  digitalWrite(led, LOW);
-  pinMode(irid_pwr_pin, OUTPUT);
-  digitalWrite(irid_pwr_pin, LOW);
-
-
-  /*Make sure a SD is available (2-sec flash led means SD card did not initialize)*/
-  while (!SD.begin(chipSelect)) {
-    digitalWrite(led, HIGH);
-    delay(2000);
-    digitalWrite(led, LOW);
-    delay(2000);
-  }
-
-  read_params();
-  
-  /*Make sure RTC is available*/
-  while (!rtc.begin())
-  {
-    digitalWrite(led, HIGH);
-    delay(500);
-    digitalWrite(led, LOW);
-    delay(500);
-  }
-
-  mySDI12.begin();  //Begin HYDROS21
-  // Serial.begin(9600);
-  
-  String datastring = take_measurement(rtc.now().timestamp());
-  write_to_csv(datastring + ",startup", "/DATA.csv");
-  // send_msg("startup:"+datastring, 300, 2, "F");
-  // Serial.println(datastring);
-
-  SD.remove("/TEMP.CSV");  //Remove previous daily values CSV
-
 }
 
-/*
-   Code here is repeated, sample HYDROS21 at sample interval, log to SD, and transmit hourly averages over IRIDIUM at specified intervals
-*/
-void loop(void)
-{
-  digitalWrite(led, HIGH);
-  delay(50);
-  digitalWrite(led, LOW);
-    
-  present_time = rtc.now();  //Get the present datetime from RTC
-  // Serial.println(present_time.timestamp());  
+void setup(void){
   
-  if(present_time.second() == 0 ){
-    read_params();
+  // OPTIONAL IF PLUGGED IN
+  delay(1000);
+  Serial.begin(9600);
+  Serial.println("starting");
 
-    if(present_time.minute() % sample_intvl[0] == 0){
+  // SET LED  
+  pinMode(led, OUTPUT); delay(30); digitalWrite(led, HIGH); delay(2000); digitalWrite(led, LOW);
+  pinMode(13, OUTPUT); digitalWrite(13, HIGH); delay(30); digitalWrite(13, LOW);  
+
+  // SET RELAYS
+  pinMode(SensorSetPin, OUTPUT); digitalWrite(SensorSetPin, HIGH); delay(30); digitalWrite(SensorSetPin, LOW);
+  pinMode(SensorUnsetPin, OUTPUT); digitalWrite(SensorUnsetPin, HIGH); delay(30); digitalWrite(SensorUnsetPin, LOW);
+
+  // SET IRIDIUM 
+  pinMode(IridPwrPin, OUTPUT);// delay(30); digitalWrite(IridPwrPin, LOW);
+
+  // START SDI-12 PROTOCOL
+  mySDI12.begin();
+
+  // CHECK RTC
+  while (!rtc.begin()){blinky(2,200,200,2000);}
+  
+  // CHECK SD CARD
+  while (!SD.begin(chipSelect)) {blinky(3,200,200,2000);}
+
+  // CHECK PARAM.txt
+  read_params();
+  
+  // SAMPLE ON STARTUP - SET RELAY
+  digitalWrite(SensorSetPin, HIGH); delay(30);
+  digitalWrite(SensorSetPin, LOW);
+  delay(1000); 
+
+  // SAMPLE ON STARTUP - ACTIVATE WIPER
+  // digitalWrite(WiperSetPin, HIGH); delay(20);    
+  // digitalWrite(WiperSetPin, LOW); delay(20); delay(100); // >50 ms burst of power activates the wiper's full rotation
+  // digitalWrite(WiperUnsetPin, HIGH); delay(20);    
+  // digitalWrite(WiperUnsetPin, LOW); delay(20);    
+  // delay(10000); // wait for full rotation (about 6 seconds)
+
+  // SAMPLE ON STARTUP - SAMPLE
+  String datastring = rtc.now().timestamp()+","+String(sample_hydros_M()+","+sample_batt_v());
+  Serial.println(datastring);  
+  
+  // SAMPLE ON STARTUP - WRITE
+  write_to_csv(my_header, datastring, "/DATA.csv");
+
+  // SAMPLE ON STARTUP - UNSET RELAY
+  digitalWrite(SensorUnsetPin, HIGH); delay(30);
+  digitalWrite(SensorUnsetPin, LOW);
+
+  // SAMPLE ON STARTUP - SEND MESSAGE
+  if(onstart_irid[0] == "T"){send_msg("startup: "+ datastring, 120, 1);}
+
+  SD.remove("/HOURLY.csv");  
+  
+  // TEST IRID MSG FORMAT
+  // write_to_csv(my_header, datastring, "/HOURLY.csv");
+  // write_to_csv(my_header, datastring, "/HOURLY.csv");
+  // write_to_csv(my_header, datastring, "/HOURLY.csv");
+  // write_to_csv(my_header, datastring, "/HOURLY.csv");
+  // write_to_csv(my_header, datastring, "/HOURLY.csv");
+  // datastring = prep_msg();
+  // Serial.println(datastring);
+}
+
+void loop(void){
+
+  // WAKE UP, WHAT TIME? 
+  present_time = rtc.now();
+  Serial.println(rtc.now().timestamp());
+
+  // BLINK IF INTERVAL OF 10s
+  if(present_time.second() % 10 == 0){
+    blinky(1,20,200,200);
+
+    // SAMPLE IF AT INTERVAL AND ON 0s
+    if(present_time.minute() % sample_freq_m_32 == 0 & present_time.second() == 0){    
+        
+      // SAMPLE - SET RELAY 
+      digitalWrite(SensorSetPin, HIGH); delay(30);
+      digitalWrite(SensorSetPin, LOW);
+      delay(1000); 
+
+      // SAMPLE - SAMPLE 
+      String datastring = present_time.timestamp()+","+String(sample_hydros_M()+","+sample_batt_v());
+      Serial.println(datastring);
+
+      // SAMPLE - WRITE TO CSV 
+      write_to_csv(my_header, datastring, "/DATA.csv");
       
-      String datastring = take_measurement(present_time.timestamp());
-      write_to_csv(datastring,"/DATA.csv");
-      // Serial.println(datastring);
+      // SAMPLE - UNSET RELAY
+      digitalWrite(SensorUnsetPin, HIGH); delay(30);
+      digitalWrite(SensorUnsetPin, LOW);
 
-        if(present_time.minute() == 0){
-          write_to_csv(datastring,"/TEMP.csv"); 
-
-          if(present_time.hour() % irid_freq[0] == 0){
-            String irid_datastring = irid_temp_to_irid();
-            send_msg(irid_datastring, 300, 2, "F");
-            write_to_csv(datastring+",irid","/HIST.csv"); 
-            // Serial.println(irid_datastring);
-            SD.remove("/TEMP.CSV");  //Remove previous daily values CSV
+      // SAVE TO HOURLY IF ON THE HOUR
+      if(present_time.minute() == 0){
+        write_to_csv(my_header, datastring, "/HOURLY.csv");
           
-          }
+        // SEND MESSAGE IF ON IRIDIUM INTERVAL
+        if(present_time.hour() % irid_freq_h_32 == 0){
+          datastring = prep_msg();
+          send_msg(datastring, 300, 2); 
+          SD.remove("/HOURLY.csv");
+           
         }
       }
-      
     }
-
-  if(rtc.now().second() < 56){
-    // delay(4000);
-    LowPower.sleep(4000);
-    }else{
-      // delay(1000);
-      LowPower.sleep(1000);
-      }
-
+    
+    // Sleep until 10 secs interval 0,10,20,...
+    DateTime sample_end = rtc.now();
+    uint32_t sleep_time = ((10-(sample_end.second()%10))*1000.0)-500;
+    Serial.println(sleep_time);
+    // delay(sleep_time);
+    LowPower.sleep(sleep_time); 
+    
+  }  
+  delay(300);
 }
