e5f400b63e580b105c7988fec6035684d0e97906
Rachel Pagdin <rachelerpagdin@gmail.com>
2024-06-05 13:40:31 -0700

    fix errors

--- v0.5_libraried/library_v0.2/RemoteLogger.h
+++ v0.5_libraried/library_v0.2/RemoteLogger.h
@@ -23,19 +23,39 @@ class RemoteLogger
         
         RemoteLogger();         // basic constructor
 
+        /* BASIC UNIT FUNCTIONS */
         void blinky(int16_t n, int16_t high_ms, int16_t low_ms, int16_t btw_ms);
         void write_to_csv(String header, String datastring_for_csv, String outname);
         float sample_batt_v();
         void tpl_done();
+
+        /* TELEMETRY */
         int send_msg(String my_msg);    // send message over Iridium
+        void irid_test(String msg);               // test Iridium modem (sends message)
+
+        /* SAMPLING FUNCTIONS */
+        String sample_hydros_M(SDI12 bus, int sensor_address);
+
+        /* PIN ASSIGNMENT SETTERS */
+        void setLedPin(byte pin);
+        void setBattPin(byte pin);
+        void setTplPin(byte pin);
+        void setIridSlpPin(byte pin);
+        void setSDSelectPin(byte pin);
+        //void setDataPin(byte pin);
+
+        RTC_PCF8523 rtc;
+    
+    private:
+
+        void sync_clock();      // sync RTC to Iridium time
+
+        IridiumSDB modem;
 
         byte ledPin = 8;        // built-in green LED pin on Feather M0 Adalogger - can modify for other boards
         byte vbatPin = 9;          // built-in battery pin on Feather M0 Adalogger - can modify for other boards
         byte tplPin = A0;           // attach TPL to A0 (only analog output pin on Adalogger)
         byte IridSlpPin = 13;           // attach Irid sleep pin (7 - grey) to pin 13 - can modify to other digital pin
-
-    
-    private:
-
-        void sync_clock();
+        byte chipSelect = 4;          // SD select pin is 4 on Feather M0 Adalogger - can modify for other boards
+        //byte dataPin = 12;              // attach SDI-12 data line to pin 12 
 }
